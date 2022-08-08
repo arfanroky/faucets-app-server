4265eb5318273d8a2b8b0467b69b639bc940a92f
Your Name <you@example.com>
2022-08-08 13:17:13 +0600

    fix the userInfo getting issue

--- index.js
+++ index.js
@@ -9,44 +9,38 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(express.json());
 app.use(cors());
 
-
-
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.zxhv2tb.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-  
 const run = async () => {
-    try {
-        client.connect()
-        const userDataCollection = client.db('faucets_app').collection('users');
+  try {
+    await client.connect();
+    const userDataCollection = client.db('faucets_app').collection('users');
 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const userInfo = req.body;
+      console.log(userInfo);
+      const filter = { email: email };
+      const options = { upsert: true };
+      const doc = { $set: userInfo };
 
-        app.put('/user/:email', async(req, res) => {
-            const email = req.params.email;
-            const userInfo = req.body;
-            const filter = {email: email};
-            const options = {upsert: true};
-            const doc = {$set: userInfo};
-
-            const result = await userDataCollection.updateOne(filter, doc, options);
-            res.send({message: 'Successfully created ', result});
-
-        })
-
-        
-    } catch (error) {
-        console.log(error);
-    }
-}
+      const result = await userDataCollection.updateOne(filter, doc, options);
+      res.send({ message: 'Successfully created ', result });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('Hello from server')
-})
+  res.send('Hello from server');
+});
 
-
-
-
-app.listen(port, () => console.log('Listening on the port', port))
+app.listen(port, () => console.log('Listening on the port', port));
